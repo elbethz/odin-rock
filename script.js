0e24471f38d116c77297ddd8137b90b93790b0f2
elbethz <brucaruf@protonmail.com>
2024-05-27 22:02:50 -0300

    fixed bugs and added the play function

--- script.js
+++ script.js
@@ -2,40 +2,83 @@
 let humanScore = 0;
 let computerScore = 0;
 
-//user player function
-let userChoice = () => prompt("ingresa tu opcion: ");
-
-//random computer choice function:
-
-
-
+//function to generate a random computer choice
 function getComputerChoice(){
   let randomNum = Math.floor(Math.random()*3);
 
   switch (randomNum){
     case 0:
-      return "rock";
+      return "rock"
       break;
 
     case 1:
-      return "paper";
+      return "paper"
       break;
 
     case 2:
-      return "scissors";
+      return "scissors"
       break;
   }
-
 }
 
-function play(computerChoice,humanChoice){
-  humanChoice = userChoice().toLowerCase();
-  computerChoice = getComputerChoice().toLowerCase();
-  console.log("human says: " + humanChoice);
-  console.log("computer says: " + computerChoice);
-  console.log("computer: " + computerScore);
-  console.log("human: " + humanScore);
+
+//function to take user input
+function getUserChoice(){
+  let playerChoice = prompt("enter your play: ");
+  return playerChoice.trim();
 }
 
-play()
+//store the function in variables for use them in the play() function
+let humanPlayer = getUserChoice().toLowerCase();
+let computerPlayer = getComputerChoice();
+console.log("humanPlayer: " + humanPlayer);
+console.log("computerPlayer: " + computerPlayer);
+
+//playfunction whit all the game logic
+function play(human,computer){
+  if(humanPlayer === "rock" && computerPlayer === "scissors"){
+    humanScore++;
+    return "human player wins.. rocks destroy scissors!!"
+  }
+  else if(humanPlayer === "paper" && computerPlayer === "rock"){
+    humanScore++;
+    console.log("human score: " + humanScore);
+    console.log("computer score: " + computerScore);
+    return "human player wins.. paper grabs rock!!"
+  }
+  else if(humanPlayer === "scissors" && computerPlayer === "paper"){
+    humanScore++;
+    console.log("human score: " + humanScore);
+    console.log("computer score: " + computerScore);
+    return "human player wins.. scissors cut the paper"
+  }
+  else if(humanPlayer === "rock" && computerPlayer === "paper"){
+    computerScore++;
+    console.log("human score: " + humanScore);
+    console.log("computer score: " + computerScore);
+    return "computer  wins.. paper grabs the rock!!"
+  }
+  else if(humanPlayer === "scissors" && computerPlayer === "rock"){
+    computerScore++;
+    console.log("human score: " + humanScore);
+    console.log("computer score: " + computerScore);
+    return "computer  wins.. rocks destroy scissors!!"
+  }
+  else if(humanPlayer === "paper" && computerPlayer === "scissors"){
+    computerScore++;
+    console.log("human score: " + humanScore);
+    console.log("computer score: " + computerScore);
+    return "computer wins.. scissors cut the paper"
+  }
+  else if(humanPlayer === computerPlayer){
+    console.log("human score: " + humanScore);
+    console.log("computer score: " + computerScore);
+    return "its a tie!!!"
+  }
+}
+
+let result = play(humanPlayer, computerPlayer);
+console.log(result);
+
+
 
